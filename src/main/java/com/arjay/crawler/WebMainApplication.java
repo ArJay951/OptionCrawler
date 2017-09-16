6a54163f6aad54dd7533cafe11e4827b6c3f6db4
BuildTools <unconfigured@null.spigotmc.org>
2017-09-17 01:28:19 +0800

    fix error.

--- src/main/java/com/arjay/crawler/WebMainApplication.java
+++ src/main/java/com/arjay/crawler/WebMainApplication.java
@@ -2,7 +2,6 @@ package com.arjay.crawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -19,11 +18,6 @@ public class WebMainApplication {
 		executor.setKeepAliveSeconds(30);
 		return executor;
 	}
-=======
-
-@SpringBootApplication
-public class WebMainApplication {
->>>>>>> branch 'master' of https://github.com/ArJay951/OptionCrawler.git
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebMainApplication.class);
